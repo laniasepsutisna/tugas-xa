bebee4d29d21afe4aa28d60466ee6dca10723a04
Lani Asep Sutisna <laniasep.sutisna@gmail.com>
2021-03-18 22:20:14 +0700

    orders.json & orders-detail.json

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Webpatser\Uuid\Uuid;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class OrderController extends Controller
 {
+    public function __construct()
+    {
+        $this->path = public_path('data-json/orders.json'); //get data products-backup.json
+        $this->orders = json_decode(file_get_contents($this->path), true);
+    }
+
     /**
      * Assign mass data variable.
      *
